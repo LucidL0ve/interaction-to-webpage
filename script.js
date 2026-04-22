28203522f3c2c8ef3c9a2db332e1914da23fe4d2
LucidL0ve <izukamidoriya86@gmail.com>
2026-04-21 20:10:51 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,30 @@
+document.addEventListener("DOMContentLoaded", () => {
+  const gallery = document.getElementById("gallery");
+  const toggleImageButton = document.getElementById("toggleImage");
+  const changeTextButton = document.getElementById("changeTextButton");
+  const paragraph = document.getElementById("myDIV");
 
+  let toggled = false;
+
+  
+  toggleImageButton.addEventListener("click", () => {
+    if (toggled) {
+      gallery.src = "./assets/image1.jpg"; 
+      toggled = false;
+    } else {
+      gallery.src = "./assets/image2.jpg"; 
+      toggled = true;
+    }
+  });
+
+ 
+  changeTextButton.addEventListener("click", () => {
+    if (toggled) {
+      paragraph.textContent = "Tsukasa Tenma at your service with some new webpage designs!";
+      toggled = false;
+    } else {
+      paragraph.textContent = "Woahh!! Brand new text?? Must be the genius work of our director, Rui Kamishiro!!!";
+      toggled = true;
+    }
+  });
+});
